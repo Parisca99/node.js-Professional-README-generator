9372e28d256df3177c8f917c76ce5d67f407d595
Paris Burke <92965776+Parisca99@users.noreply.github.com>
2022-01-09 18:42:08 -0800

    latest version of index.js

--- potential-enigma-main/potential-enigma-main/Develop/index.js
+++ potential-enigma-main/potential-enigma-main/Develop/index.js
@@ -1,13 +1,90 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create a function to initialize app
-function init() {}
+const questions = [
+    {
+        type: 'input',
+        message: "What is the name of your project?",
+        name: 'title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "who contributed to this project? List github usernames.",
+        name: 'repo',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("github usernames are required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'The Unlicense'],
+        name: 'license'
+    }
+];
 
-// Function call to initialize app
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Your README.md file has been generated")
+    });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
+
+async function init() {
+    try {
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+    
+        var userInfo = userResponses.username;
+
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 init();
