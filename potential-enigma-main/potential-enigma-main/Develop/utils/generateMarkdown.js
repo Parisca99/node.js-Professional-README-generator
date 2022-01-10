610acef47229a1e1c7f22708febb2dcb8a351361
Paris Burke <92965776+Parisca99@users.noreply.github.com>
2022-01-09 18:43:24 -0800

    latest version

--- potential-enigma-main/potential-enigma-main/Develop/utils/generateMarkdown.js
+++ potential-enigma-main/potential-enigma-main/Develop/utils/generateMarkdown.js
@@ -1,20 +1,43 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(userResponses, userInfo) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  let draftMarkdown = 
+  ` ${userResponses.title}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+  
+  ${userResponses.description}
+  `
+ 
+  draftMarkdown += `
 `;
+  
+
+  if (userResponses.installation !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  
+  ${userResponses.installation}`
+  };
+  
+
+  if (userResponses.usage !== '') {
+  
+  draftMarkdown +=
+  
+  `
+
+  
+  ${userResponses.usage}`
+  };
+  draftMarkdown +=
+  `
+
+  
+  ${userResponses.license}
+  `;
+  
 }
 
 module.exports = generateMarkdown;
+
